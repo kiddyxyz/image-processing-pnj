9eccaedbc3c650ad3f4d04b3ced518b631da2887
Kiddy <kiddydhana@gmail.com>
2019-03-18 22:25:25 +0700

    Fixed task 2

--- task/task2.py
+++ task/task2.py
@@ -1,16 +1,10 @@
 import numpy, cv2 as cv, os
 
 #img = numpy.zeros((11300, 11400), dtype=numpy.uint8)
-img = numpy.zeros((230,230), dtype=numpy.uint8)
+#img = numpy.zeros((230,230), dtype=numpy.uint8)
 #image = img.reshape(300,400)
 #image2 = img.shape
-
-randomByteArray =  bytearray([
-    255,255,255,0,0,0,255,255,255
-],)
-flatNumpyArray = numpy.array(randomByteArray)
-print(flatNumpyArray)
-
-# grayImage2 = flatNumpyArray.reshape(300,400)
-
-cv.imwrite('RandomRunyeks.png', flatNumpyArray)
+# Create a black image
+img = numpy.zeros((212,212,3), numpy.uint8)
+cv.line(img,(20,110),(490,110),(255,255,255),5)
+cv.imwrite('white-line.png', img)
